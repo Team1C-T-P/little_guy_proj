4fb461868bb3b9f27f38332a23c0ee24c684a3af
Lou Catibog <loucatibog30@gmail.com>
2026-04-29 19:48:54 +0100

    tests added for getItemsByType for shop_view.dart

--- test/views/shop_view_test.dart
+++ test/views/shop_view_test.dart
@@ -39,4 +39,37 @@ void main() {
     });
   });
   // test getItemsByType
+  group('getItemsByType', () {
+    test('return only hats when selected type is hat', () async {
+      await TestDatabase.seedHat(db, name: 'Top Hat');
+      await TestDatabase.seedFood(db, name: 'Bread');
+      final hats = await shopDb.getItemsByType('hat');
+      expect(hats.length, 1);
+      expect(hats.first['item_name'], 'Top Hat');
+      expect(hats.first['type'], 'hat');
+    });
+
+    test('returns only food when selected type is food', () async {
+      await TestDatabase.seedHat(db, name: 'Top Hat');
+      await TestDatabase.seedFood(db, name: 'Bread');
+      final food = await shopDb.getItemsByType('food');
+      expect(food.length, 1);
+      expect(food.first['item_name'], 'Bread');
+      expect(food.first['type'], 'food');
+    });
+
+    test('returns empty list when no items of selected type exists', () async {
+      await TestDatabase.seedHat(db);
+      final food = await shopDb.getItemsByType('food');
+      expect(food, isEmpty);
+    });
+
+    test('return multiple items with the same type', () async {
+      await TestDatabase.seedHat(db, name: 'Top Hat');
+      await TestDatabase.seedHat(db, name: 'Witch Hat');
+      await TestDatabase.seedHat(db, name: 'Sun Hat');
+      final hats = await shopDb.getItemsByType('hat');
+      expect(hats.length, 3);
+    });
+  });
 }
