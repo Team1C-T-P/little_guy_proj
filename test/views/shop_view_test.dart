655aa5ea901544d9ccfd89f8b7ce07cbb2878daf
Lou Catibog <loucatibog30@gmail.com>
2026-04-29 19:18:48 +0100

    added unit test for getUserCurrency function in shop_view.dart

--- test/views/shop_view_test.dart
+++ test/views/shop_view_test.dart
@@ -13,5 +13,30 @@ void main() {
   setUp(() async {
     db = await TestDatabase.createFresh();
     shopDb = ShopDatabase(db);
-  })
+  });
+
+  tearDown(() async {
+    await db.close();
+  });
+
+  // test getUserCurrency
+  group('getUserCurrency', () {
+    test('returns correct currency for a user', () async {
+      await TestDatabase.seedUser(db, currency: 500);
+      final currency = await shopDb.getUserCurrency(1);
+      expect(currency, 500);
+    });
+
+    test('returns 0 if a user doesnt exist', () async {
+      final currency = await shopDb.getUserCurrency(9);
+      expect(currency, 0);
+    });
+
+    test('returns 0 currency as the user is broke', () async {
+      await TestDatabase.seedUser(db, currency: 0);
+      final currency = await shopDb.getUserCurrency(1);
+      expect(currency, 0);
+    });
+  });
+  // test getItemsByType
 }
