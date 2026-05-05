8128aec6bad4cff11aa85a77dc1bab67b1c4021c
anirejlova <anna8080@post.cz>
2026-05-05 09:29:03 +0100

    Fix bg colour, comment out unnecessary code, idea for optimisation

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:pedometer/pedometer.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:pedometer/pedometer.dart'; // no need?
+// import 'package:permission_handler/permission_handler.dart'; // why
 import 'package:flutter_flame_playground/utils/step_counter.dart';
+import 'package:flutter_flame_playground/models/step_points_service.dart';
 import 'package:gap/gap.dart';
 import 'package:flutter_flame_playground/little%20guy.dart';
 import 'package:flutter_flame_playground/widgets/button.dart';
-import 'package:flutter_flame_playground/models/pet_maintainment_database.dart';
+import 'package:flutter_flame_playground/models/pet_maintainment_database.dart'; // use step_points_service instead?
 
 class ProfileScreen extends StatefulWidget {
   @override
   _ProfileState createState() => _ProfileState();
 }
 
+//instead of getting data from pet_maintainment_database can we get it from step points service? - as there is a summary.
 class _ProfileState extends State<ProfileScreen> {
   String _userName = "";
   String _petName = "";
@@ -38,17 +40,17 @@ class _ProfileState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 250, 255, 251),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(219, 150, 242, 176),
-        title: const Text('Profile Page'),
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: const Color.fromARGB(219, 150, 242, 176),
+      //   title: const Text('Profile Page'),
+      // ),
       body: Column(
         children: <Widget>[
           Expanded(
             flex: 10,
             child: Container(
               alignment: Alignment.bottomCenter,
-              color: Color.fromARGB(255, 250, 255, 251),
+              color: Color.fromARGB(255, 221, 249, 255),
               child: Center(child: LittleGuy()),
             ),
           ),
@@ -88,17 +90,17 @@ class _ProfileState extends State<ProfileScreen> {
                                         ),
                                       ),
                                     ),
-                                    Container(child: Text(" | ")),
-                                    Container(child: Text(" LVL 1 ")),
+                                    Container(child: Text(" --- ")),
+                                    Container(child: Text("[Currency]")),
                                   ],
                                 ),
                               ),
-                              const Align(
+                              Align(
                                 alignment: Alignment.topLeft,
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
-                                    Text("Total Steps:  "),
+                                    Text("Total Steps: "),
                                     Text("Items Collected:  "),
                                     Text("Total Friends:  "),
                                   ],
