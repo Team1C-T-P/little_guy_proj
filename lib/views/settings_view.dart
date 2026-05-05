e20da3f02137404f8c460f40eec160dfe4b1c1c8
anirejlova <anna8080@post.cz>
2026-05-05 09:31:11 +0100

    Add appbar to settings_view.dart

--- lib/views/settings_view.dart
+++ lib/views/settings_view.dart
@@ -43,6 +43,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Settings Screen')),
       backgroundColor: Color.fromARGB(219, 173, 230, 189),
       body: SingleChildScrollView(
         child: Column(
